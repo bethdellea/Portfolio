70662c59156063a4a1342369ee5a51623eaec4d1
Claire Riley <claireriley001@gmail.com>
2016-09-18 22:31:45 -0700

    Updates to center and fix spacing

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,18 +1,9 @@
-$(document).ready(function(){
-						   
- $(window).resize(function(){
-
-  $('.bg').css({
-   position:'absolute',
-   left: ($(window).width() 
-     - $('.bg').outerWidth())/2,
-   top: ($(window).height() 
-     - $('.bg').outerHeight())/2
-  });
-		
- });
- 
- // To initially run the function:
- $(window).resize();
-
+$(function() {
+    $('.bg').css({
+        'position' : 'absolute',
+        'left' : '50%',
+        'top' : '50%',
+        'margin-left' : function() {return -$(this).outerWidth()/2},
+        'margin-top' : function() {return -$(this).outerHeight()/2}
+    });
 });
