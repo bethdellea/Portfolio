d2d6e55b1556974c9b0affeab65ac4e8b03c091d
Claire Riley <claireriley001@gmail.com>
2016-09-25 19:19:17 -0700

    Font Sizing Fixes and Navigation

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,3 +10,37 @@ $(function() {
 $(document).ready(function() {
     $(".fancybox").fancybox();
 });
+$(function(){
+  $('a').each(function() {
+    if ($(this).prop('href') == window.location.href) {
+      $(this).addClass('current');
+    }
+  });
+});
+var autoSizeText;
+autoSizeText = function() {
+  var el, elements, _i, _len, _results;
+  elements = $('.resize');
+  console.log(elements);
+  if (elements.length < 0) {
+    return;
+  }
+  _results = [];
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+    el = elements[_i];
+    _results.push((function(el) {
+      var resizeText, _results1;
+      resizeText = function() {
+        var elNewFontSize;
+        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+        return $(el).css('font-size', elNewFontSize);
+      };
+      _results1 = [];
+      while (el.scrollHeight > el.offsetHeight) {
+        _results1.push(resizeText());
+      }
+      return _results1;
+    })(el));
+  }
+  return _results;
+};
